51187a0372d9f097ce080b85dd3936deb20157db
Jihoon <jhc20@sfu.ca>
2012-05-31 13:17:31 -0700

    updated the script

--- querybuilder.js
+++ querybuilder.js
@@ -1,82 +1,40 @@
 /**
  * Module dependencies.
  */
-var
-    es = require('./elasticsearch'),    
-    fs = require('fs');
+var QueryBuilder =  function () {
 
-var 
-    default_port = "9200";        
-
-var port, file_name;
-
-port = process.argv[2] || default_port;
-index_name = process.argv[3];
-mapping_name = process.argv[4];
-
-
-var db = es.connect("localhost", port);
-var index = db.index(index_name);
-var mapping = index.mapping(mapping_name);
 var queries = [];
 
-//var term = [];
-//var wildcard = [];
-//var prefix = [];
-//var range = [];
-
-// time to search
-var target = mapping; //index;
-
-//var q = new Query();
-//q.range_builder("cost", "from", 1, "to", 3);
-range_builder("cost", "from", 1, "to", 4);
-range_builder("cost", "from", 1, "to", 5);	
-term_builder("name", "angel");
-
-build();
-
-function build() {
-    target.count(countCallback);
-
+this.build = function (from, size) {
+    return query_builder(from, size);
 }
 
-function query_builder(size) {
+function query_builder(from, size) {
     var q = {
         "query": {
             //"match_all": {}   /
 
-	        "bool": {
+            "bool": {
 
 
-        	}
-			          
+            }
+                      
         }
     };
+    q.from = from;
     q.size = size;
-	//q.query.bool.must = range_builder("cost", "from", 1, "to", 3);	
+    //q.query.bool.must = range_builder("cost", "from", 1, "to", 3);    
 
-	q.query.bool.must = queries;
+    q.query.bool.must = queries;
 
-	//q.query.bool.must.term = term_builder("name", "dread");	
-	//q.query.bool.must.wildcard = wildcard_builder("name", "dread");
-	//q.query.bool.must.prefix = term_builder("description", "dre");	
-
-	// make it preety
-	var str = JSON.stringify(q, null, '  ')
-    console.log("query = " + str);
+    //q.query.bool.must.term = term_builder("name", "dread");   
+    //q.query.bool.must.wildcard = wildcard_builder("name", "dread");
+    //q.query.bool.must.prefix = term_builder("description", "dre");    
     return q;
 
 }
 
-function query_appender(array) {
-	var query;
-	for (i=0; i < array.length; i++ ) {
-		
-	}
-}
-
-function bool_builder(size) {
+this.bool_builder = function(size) {
     var b = {
         "bool": {
         }
@@ -84,7 +42,7 @@ function bool_builder(size) {
     return b;
 }
 
-function must_builder(size) {
+this.must_builder = function(size) {
     var m = {
         "must": {
         }
@@ -92,130 +50,93 @@ function must_builder(size) {
     return m;
 }
 
-function range_builder(property, name1, val1, name2, val2) {	
+this.range_builder = function (property, name1, val1, name2, val2) {    
 
-	var rangeQuery = {};
+    var rangeQuery = {};
 
-	var rangeObj = {};
-	//var range = {from:start, to: finish};
-	var range = {};
-	if (name1) {
-		range[name1] = val1;
-	}	
-	if (name2) {
-		range[name2] = val2;
-	}	
+    var rangeObj = {};
+    //var range = {from:start, to: finish};
+    var range = {};
+    if (name1) {
+        range[name1] = val1;
+    }   
+    if (name2) {
+        range[name2] = val2;
+    }   
 
-	console.log(range);
-	rangeObj[property] = range;   
-	rangeQuery["range"] = rangeObj;
+    console.log(range);
+    rangeObj[property] = range;   
+    rangeQuery["range"] = rangeObj;
 
-	var str = JSON.stringify(rangeQuery);
-	console.log("range obj = " + str);
+    var str = JSON.stringify(rangeQuery);
+    console.log("range obj = " + str);
 
-	//console.log("length = " + this.range_Queries.length);
+    //console.log("length = " + this.range_Queries.length);
 
-	queries.push(rangeQuery);
+    queries.push(rangeQuery);
 
     /*
     ex)
-    var r = {			
-		cost: {
-			from: start,
-			to: finish
-		}			
+    var r = {           
+        cost: {
+            from: start,
+            to: finish
+        }           
     };
 */
 
     return rangeQuery;
 }
 
-function term_builder(property, value) {
-	var termQuery = {};
+this.term_builder = function(property, value) {
+    var termQuery = {};
 
-	var termObj = {};		
-	termObj[property] = value;
-   	termQuery["term"] = termObj;
-   	/* ex)
-	term: {
-		name: "seraph"
-	}
-	*/
+    var termObj = {};       
+    termObj[property] = value;
+    termQuery["term"] = termObj;
+    /* ex)
+    term: {
+        name: "seraph"
+    }
+    */
 
-	queries.push(termQuery);
+    queries.push(termQuery);
     return termQuery;
 }
 
-function wildcard_builder(property, value) {
-	var wildCardQuery = {};
+this.wildcard_builder = function(property, value) {
+    var wildCardQuery = {};
 
-	var wildCardObj = {};		
-	wildCard[property] = value + "*";
-	wildCardQuery["wildcard"] = wildCard;
+    var wildCardObj = {};       
+    wildCard[property] = value + "*";
+    wildCardQuery["wildcard"] = wildCard;
    
-   	/* ex)
-	wildcard: {
-		name: "drea*"
-	}
-	*/
+    /* ex)
+    wildcard: {
+        name: "drea*"
+    }
+    */
 
-	queries.push(wildCardQuery);
+    queries.push(wildCardQuery);
     return wildCardQuery;
 }
 
-function prefix_builder(property, value) {
-	var prefixQuery = {};
+this.prefix_builder = function(property, value) {
+    var prefixQuery = {};
 
-	var prefixObj = {};		
-	prefixObj[property] = value;
-	prefixQuery["prefix"] = prefixObj;
+    var prefixObj = {};     
+    prefixObj[property] = value;
+    prefixQuery["prefix"] = prefixObj;
    
-   	/* ex)
-	prefix: {
-		name: "seraph"
-	}
-	*/
+    /* ex)
+    prefix: {
+        name: "seraph"
+    }
+    */
 
-	queries.push(prefixQuery);
+    queries.push(prefixQuery);
     return prefixQuery;
 }
 
-
-function countCallback(response, obj) {
-    if (obj.count) {
-        var count = JSON.stringify(obj.count);
-        console.log("count = " + count);
-        var q = query_builder(count);        
-        target.search(q, searchCallback);
-    }
-
-
 }
-
-function searchCallback(response, obj) {
-    if (obj.hits) {
-        //TODO: Remove unused information from the JSON        )
-        //var data = JSON.stringify(obj.hits.hits);
-        //console.log("data = " + data);
-        var json_data = obj.hits.hits;
-        var count = 0;
-        json_data.forEach(function(data) {
-        	var data_source = data._source;
-        	/*
-            console.log("Set = " + data_source.set);
-            console.log("Name = " + data_source.name);
-            console.log("icon = " + data_source.icon); 
-            console.log("type = " + data_source.type);
-            console.log("cost = " + data_source.cost);
-            console.log("mechanics = " + data_source.mechanics);
-            console.log("description = " + data_source.description);
-            */
-            count++;
-        })
-
-        console.log("Result = " + count);
-    }
-}
-
-
 
